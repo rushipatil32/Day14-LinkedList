46d03898f82f2dc7692144c3eafedbecd98f35b0
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-28 15:11:33 +0530

    Added:ability to create a linked list by adding elements

--- src/com/blz/linkedlist/LinkedList.java
+++ src/com/blz/linkedlist/LinkedList.java
@@ -11,4 +11,31 @@ public class LinkedList {
 			this.data = data;
 		}
 	}
+
+	public void addFirst(Object data) {
+
+		Node newNode = new Node(data);
+		if (head == null)
+			head = newNode;
+		else {
+			newNode.ref = head;
+			head = newNode;
+		}
+	}
+
+	public void display() {
+		if (head == null)
+			System.out.println("No elements to display.");
+		else {
+			Node temp = head;
+			while (temp != null) {
+				if (temp.ref != null)
+					System.out.print(temp.data + " -> ");
+				else
+					System.out.println(temp.data);
+				temp = temp.ref;
+			}
+
+		}
+	}
 }
--- src/com/blz/linkedlist/LinkedListMain.java
+++ src/com/blz/linkedlist/LinkedListMain.java
@@ -3,6 +3,10 @@ package com.blz.linkedlist;
 public class LinkedListMain {
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
-		System.out.println("Linked List Created ");
+		System.out.println("Adding elements");
+		list.addFirst(70);
+		list.addFirst(30);
+		list.addFirst(56);
+		list.display();
 	}
 }
